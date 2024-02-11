10b0dfe7abf6fc55a4e71a2cfae5ccbbb8b28ee7
Masjoel <masjoel@gmail.com>
2024-02-11 07:48:49 +0700

    update order notification

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Notification;
 
 class OrderController extends Controller
 {
@@ -53,13 +56,26 @@ class OrderController extends Controller
      */
     public function update(Request $request, Order $order)
     {
+        $order = Order::where('id', $order->id);
         $order->update([
             'status' => $request->status,
             'shipping_resi' => $request->shipping_resi,
         ]);
+        if ($request->status == 'on_delivery') {
+            $this->sendNotificationToUser($order->first()->user_id, 'Paket Dikirim dengan nomor resi ' . $request->shipping_resi);
+        }
         return redirect()->route('order.index');
     }
-
+    public function sendNotificationToUser($userId, $message)
+    {
+        $user = User::find($userId);
+        $token = $user->fcm_id;
+        $messaging = app('firebase.messaging');
+        $notification = Notification::create('Paket Dikirim', $message);
+        $message = CloudMessage::withTarget('token', $token)
+            ->withNotification($notification);
+        $messaging->send($message);
+    }
     /**
      * Remove the specified resource from storage.
      */
