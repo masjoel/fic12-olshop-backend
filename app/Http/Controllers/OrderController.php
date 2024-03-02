8b3380b79f004adb83527cd7dbcdac9e487d159f
Masjoel <masjoel@gmail.com>
2024-03-02 21:18:03 +0700

    update orderBy order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,7 +15,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = Order::paginate(10);
+        $orders = Order::orderBy('created_at', 'desc')->paginate(10);
         return view('pages.order.index', compact('orders'));
     }
 
